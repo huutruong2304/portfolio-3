8beb7668934fb7d58adf432bbe8d3511449276ec
Truong Nguyen <38267209+huutruong2304@users.noreply.github.com>
2025-10-16 00:22:54 +0700

    feat: build ui for home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,62 @@
-import { Button } from '@/components/ui/button';
+import TwoColSection from '@/components/custom/two-col-section'
+import FacebookIcon from '@/components/icons/facebook-icon'
+import GithubIcon from '@/components/icons/github-icon'
+import LinkedinIcon from '@/components/icons/linkedin-icon'
+import YoutubeIcon from '@/components/icons/youtube-icon'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      {/* Home */}
+      <div className="app-container">
+        <TwoColSection>
+          <div className="">
+            <p className="text-gray-500">Hi I am</p>
+            <h2 className="text-lg font-semibold text-gray-400">Truong Nguyen</h2>
+            <h1 className="text-6xl font-semibold text-primary">UI/UX Designer</h1>
+
+            <div className="flex gap-4 mt-10 mx-auto">
+              <FacebookIcon className="icon-hover" />
+              <GithubIcon className="icon-hover" />
+              <LinkedinIcon className="icon-hover" />
+              <YoutubeIcon className="icon-hover" />
+            </div>
+            <div className="flex gap-4 mt-10">
+              <Button className="min-w-36"> Hire Me</Button>
+              <Button className="min-w-36" variant={'outline'}>
+                Download CV
+              </Button>
+            </div>
+
+            <div className="w-11/12 bg-card flex flex-row gap-5 p-5 mt-10  justify-between rounded-lg">
+              <div className="flex flex-col">
+                <h5 className="text-primary">5+</h5>
+                <p className="text-gray-400">Experiences</p>
+              </div>
+              <div className="flex flex-col border-l-3 border-gray-400 pl-8">
+                <h5 className="text-primary">20+</h5>
+                <p className="text-gray-400">Projects done</p>
+              </div>
+              <div className="flex flex-col border-l-3 border-gray-400 pl-8">
+                <h5 className="text-primary">80+</h5>
+                <p className="text-gray-400">Happy Clients</p>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <div className="bg-card w-2/3 aspect-square mx-auto rounded-full relative">
+              <Image
+                src={'/images/avatar.png'}
+                fill
+                className="object-contain bottom-20"
+                alt="truong nguyen avatar"
+              />
+            </div>
+          </div>
+        </TwoColSection>
+      </div>
+    </div>
+  )
 }
