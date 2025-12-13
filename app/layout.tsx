69afab4b4a36c2344e83b385ceea2800cc71a20d
Truong Nguyen <38267209+huutruong2304@users.noreply.github.com>
2025-12-13 14:02:20 +0700

    feat: add scroll to top button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,38 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
-import AppHeader from '@/components/custom/app-header';
-import AppFooter from '@/components/custom/app-footer';
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import AppHeader from '@/components/custom/app-header'
+import AppFooter from '@/components/custom/app-footer'
+import ScrollToTopButton from '@/components/custom/scroll-top-button'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
-});
+})
 
 export const metadata: Metadata = {
   title: 'Hi I am Truong - Full Stack Developer',
   description: 'This is my portfolio',
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
         <AppHeader />
         {children}
+        <ScrollToTopButton />
         <AppFooter />
       </body>
     </html>
-  );
+  )
 }
